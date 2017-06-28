fdbf728db000951c6e0fcbc106b517a775e5159c
Giuseppe <giuse.scara@gmail.com>
2017-06-28 21:56:17 +0200

    Modify cors

--- rest-server/app.js
+++ rest-server/app.js
@@ -28,7 +28,7 @@ var app = express();
 // Secure traffic only
 app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "*");
   console.log('req start: ', req.secure, req.hostname, req.url, app.get('port'));
   /*if (req.secure) {
     return next();
